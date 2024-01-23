b4d7f5e91e9605362f0c62e05f67c820ba1e102e
Ruslan <veb3solidity@gmail.com>
2024-01-23 19:15:07 +0200

    less2_17

--- bord2.py
+++ bord2.py
@@ -3,8 +3,11 @@ from aiogram.types import (
     ReplyKeyboardMarkup,    #крепится под полем ввода
     KeyboardButton,         #ReplyKeyboardMarkup - кнопки для ReplyKeyboardMarkup
     InlineKeyboardMarkup,   #крепится под самими сообщениями
-    InlineKeyboardButton    #кнопки для InlineKeyboardMarkup
+    InlineKeyboardButton,    #кнопки для InlineKeyboardMarkup
+    KeyboardButtonPollType,   #для создания викторины (quiz) или голосования(regular)
 )
+# для работы с Билдером
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 #основная клавиатура для команды start
 main_kb = ReplyKeyboardMarkup(
@@ -22,16 +25,61 @@ main_kb = ReplyKeyboardMarkup(
     ],
     resize_keyboard = True,  #кнопки адаптируются под кнопки телефона - стают маленькими
     one_time_keyboard = True, # клава скрывается после первого использования
-    input_field_placeholder = "Выдерите действие с меню", # текст в меню для активации клавы
+    input_field_placeholder = "Выберите действие с меню", # текст в меню для активации клавы
     selective = True, # для чатов, клава активируется только у того кто ее вызвал???
 )
 
+
+# обработчик "ссылки"
 links_kb = InlineKeyboardMarkup(
     inline_keyboard = [
         [
             InlineKeyboardButton(text = 'Instagram', url = "https://www.instagram.com/chemodan_blog/"),
             InlineKeyboardButton(text = 'Youtube', url = "https://www.youtube.com/channel/UCWFU4cESpwDhRS-zQzSorJg"),
-            # InlineKeyboardButton(text = 'Telegram', url = "tg://resolve?dbmain=@CosmoIryna")
+            InlineKeyboardButton(text = 'Telegram', url = "tg://resolve?domain=CosmoIryna")
+            #  t.me://CosmoIryna    -- откроется в браузере
+            #  tg://resolve?domain=CosmoIryna    -- откроется в телеграм
         ]
     ]
 )
+
+# обработчик "спец.кнопки"
+spec_kb = ReplyKeyboardMarkup(
+    keyboard = [
+        [
+            KeyboardButton(text = 'Отправить ГЕО', request_location = True),  #request_location - отправлять геолокацию
+            KeyboardButton(text = 'Отправить контакт', request_contact = True),  #request_contact - отправлять контакт
+            KeyboardButton(text = 'Создать викторину', request_poll = KeyboardButtonPollType()),
+            #request_poll = KeyboardButtonPollType() - создать викторину или голосование
+            #request_poll = KeyboardButtonPollType("quiz") - создать викторину
+            #request_poll = KeyboardButtonPollType("regular") - создать голосование
+
+        ],
+        [
+            KeyboardButton(text="НАЗАД"),
+        ]
+    ],
+    resize_keyboard = True
+)
+
+# билдэр всегда возращается функцией
+# билдээр калькулятора
+def calc_kb():
+    items = [
+        "1", "2", "3", "/",
+        "4", "5", "6", "*",
+        "7", "8", "9", "-",
+        "0", ".", "=", "+",
+    ]
+    builder = ReplyKeyboardBuilder()
+    # создаем кнопки
+    # for item in items:
+    #     builder.button(text=item)
+    # или обраьтмся к генератору
+    [builder.button(text=item) for item in items]
+    builder.button(text="НАЗАД")
+    # для контроля сколько кнопок должно быть в каком ряду
+    builder.adjust(4,4,4,4) # в каждом ряду по 4 кнопки
+
+    return builder.as_markup(resize_keyboard=True)
+    # resize_keyboard=True  -- что бы все кнопки стали маленькими
--- main2.py
+++ main2.py
@@ -6,7 +6,8 @@ from aiogram.types import Message
 #импортируем клаву
 import bord2
 
-token = '6376558301:AAHa1nHkRWa-KWcAoMzaZ-xFq8FiSh8Y1M0'
+token = '6665446931:AAF2haWZeMD8BZdiIR0eJWRZQTVIdGfWFk8'
+# token = '6376558301:AAHa1nHkRWa-KWcAoMzaZ-xFq8FiSh8Y1M0'
 bot = Bot(token, parse_mode = "HTML")
 dp = Dispatcher()
 
@@ -15,13 +16,15 @@ async def start(message: Message):
     await message.answer("Hello!!!", reply_markup = bord2.main_kb)
 
 #Обработка текста с кнопок стартового меню
-
-
 @dp.message()
 async def echo(message: Message):
-    msg = message.text.lower()
+    msg = message.text.lower()      # все проверки проводить после этого только в нижнем регистре
     if msg == 'ссылки':
         await message.answer("Вот Вам ссылки на мои социальные сети!", reply_markup = bord2.links_kb)
+    if msg == 'спец.кнопки':
+        await message.answer("Специальные кнопки!", reply_markup = bord2.spec_kb)
+    if msg == 'калькулятор':
+        await message.answer("Ваш персональный калькулятор!", reply_markup = bord2.calc_kb())
 
 
 
